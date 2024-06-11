7925c30c22e47fcc5c0884c5f1bc2c7ecf5b08c6
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2024-06-11 01:36:10 -0700

    Update dependencies

--- subjektify.config.ts
+++ subjektify.config.ts
@@ -3,10 +3,7 @@ import { SubjektifyConfig } from "subjektify";
 const config: SubjektifyConfig = {
     namespace: "psychic-carnival",
     version: "0.0.1",
-    license: "MIT",
-    sources: [
-        "subjects"
-    ]
+    license: "MIT"
 }
 
 export default config;
